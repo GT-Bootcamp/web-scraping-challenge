691c56324e5b73ff95b508b1a6ec50dcea1dd95f
James Hasty <jameshasty22@gmail.com>
2020-09-14 14:09:00 -0400

    final homework

---
+++ Homework/Instructions/Mission_to_Mars/app.py
@@ -0,0 +1,8 @@
+from flask import Flask 
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask_pymongo import PyMongo
+import marsscrape
+from pymongo import MongoClient
+import pymongo
